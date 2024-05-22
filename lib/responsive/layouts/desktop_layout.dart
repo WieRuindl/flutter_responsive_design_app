e57901043b68652edbe89ebc1b87a84864bc7b8a
bepa <pavel.belov@zuhlke.com>
2024-05-22 14:10:09 +0300

    updated both tablet and desktop layouts

--- lib/responsive/layouts/desktop_layout.dart
+++ lib/responsive/layouts/desktop_layout.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_responsive_design_app/components/my_drawer.dart';
 
 class DesktopLayout extends StatelessWidget {
   const DesktopLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        iconTheme: IconThemeData(color: Colors.grey[300]),
+      ),
+      backgroundColor: Colors.green[300],
+      body: Row(
+        children: [
+          // open drawer
+          myDrawer,
+          Expanded(
+            child: Container(
+              color: Colors.blue[300],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/responsive/layouts/tablet_layout.dart
+++ lib/responsive/layouts/tablet_layout.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_responsive_design_app/components/my_drawer.dart';
 
 class TabletLayout extends StatelessWidget {
   const TabletLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.green,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        iconTheme: IconThemeData(color: Colors.grey[300]),
+      ),
+      backgroundColor: Colors.green[300],
+      drawer: myDrawer,
     );
   }
 }
