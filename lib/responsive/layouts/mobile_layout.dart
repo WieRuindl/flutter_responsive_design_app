dbfad68f7a6fb41d81c7a325d00a4cf74cfc23b4
bepa <pavel.belov@zuhlke.com>
2024-05-22 13:44:43 +0300

    created mobile layout

--- lib/responsive/layouts/mobile_layout.dart
+++ lib/responsive/layouts/mobile_layout.dart
@@ -5,8 +5,36 @@ class MobileLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.red,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        iconTheme: IconThemeData(color: Colors.grey[300]),
+      ),
+      backgroundColor: Colors.grey[300],
+      drawer: Drawer(
+        backgroundColor: Colors.grey[300],
+        child: const Column(children: [
+          DrawerHeader(
+            child: Icon(Icons.favorite),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text("D A S H B O A R D"),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("P R O F I L E"),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text("S E T T I N G S"),
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text("L O G O U T"),
+          ),
+        ]),
+      ),
     );
   }
 }
