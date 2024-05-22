fd3e910b5d1d6ade7c348a7047ac85d0120cb4cf
bepa <pavel.belov@zuhlke.com>
2024-05-23 00:24:11 +0300

    updated tablet layout

--- lib/responsive/layouts/tablet_layout.dart
+++ lib/responsive/layouts/tablet_layout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_responsive_design_app/components/my_box.dart';
 import 'package:flutter_responsive_design_app/components/my_drawer.dart';
+import 'package:flutter_responsive_design_app/components/my_tile.dart';
 
 class TabletLayout extends StatelessWidget {
   const TabletLayout({super.key});
@@ -11,8 +13,28 @@ class TabletLayout extends StatelessWidget {
         backgroundColor: Colors.grey[900],
         iconTheme: IconThemeData(color: Colors.grey[300]),
       ),
-      backgroundColor: Colors.green[300],
+      backgroundColor: Colors.grey[300],
       drawer: myDrawer,
-    );
-  }
+      body: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 4,
+            child: SizedBox(
+              width: double.infinity,
+              child: GridView.builder(
+                itemCount: 4,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+                itemBuilder: (context, index) => const MyBox(),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 6,
+              itemBuilder: (context, index) => const MyTile(),
+            ),
+          ),
+        ],
+      ),
+    );  }
 }
